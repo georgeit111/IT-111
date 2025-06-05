f1637cc34ae1a51247922361d06959af9d6a7fb5
georgeit111 <george.mtonga@seattlecolleges.edu>
2025-06-04 22:31:16 -0700

    Add files via upload

---
+++ week8/src/package_person/PersonDetails.java
@@ -0,0 +1,93 @@
+package package_person;
+
+public class PersonDetails {
+//    this file will have the main method
+
+    public static void main(String[] args) {
+
+Person firstPerson;
+Person secondPerson;
+Person thirdPerson;
+
+// in the process of creating an object from my Person class
+
+        firstPerson = new Person();
+        secondPerson = new Person();
+        thirdPerson = new Person();
+
+//        we will be entering the info, assigning values to our fields
+
+        firstPerson.name = "Cathy";
+        firstPerson.gender = 'F';
+        firstPerson.age = 33;
+        firstPerson.car = "Volkswagen Bug";
+        firstPerson.violations = false;
+        firstPerson.creditScore = 690;
+
+
+
+        secondPerson.name = "Kendall";
+        secondPerson.gender = 'M';
+        secondPerson.age = 22;
+        secondPerson.car = "Mustang";
+        secondPerson.violations = true;
+        secondPerson.creditScore = 670;
+
+
+        thirdPerson.name = "Arial";
+        thirdPerson.gender = 'F';
+        thirdPerson.age = 54;
+        thirdPerson.car = "Lexus";
+        thirdPerson.violations = true;
+        thirdPerson.creditScore = 600;
+
+
+        double monthlyRate = 0;
+        double adjustRate = 0;
+
+//        System.out.println(firstPerson.name);
+//        System.out.println(firstPerson.gender);
+//        System.out.println(firstPerson.age);
+//        System.out.println(firstPerson.car);
+//        System.out.println(firstPerson.violations);
+//        System.out.println(firstPerson.creditScore);
+//        System.out.println();
+//
+//        System.out.println(secondPerson.name);
+//        System.out.println(secondPerson.gender);
+//        System.out.println(secondPerson.age);
+//        System.out.println(secondPerson.car);
+//        System.out.println(secondPerson.violations);
+//        System.out.println(secondPerson.creditScore);
+
+
+        firstPerson.display();
+        System.out.println("Preliminary Rate for " +firstPerson.name+ ": " +firstPerson.getRate(monthlyRate)+ " Dollars");
+            System.out.println("Adjustments: " +firstPerson.assumeGender(adjustRate) + " Dollars");
+            System.out.print("Here is " +firstPerson.name+ "'s total monthly premium: ");
+            System.out.println(firstPerson.assumeGender(adjustRate) + firstPerson.getRate(monthlyRate));
+            System.out.println();
+
+
+        secondPerson.display();
+        System.out.println("Adjustments: " +secondPerson.assumeGender(adjustRate) + " Dollars");
+            System.out.println("Preliminary Rate for " +secondPerson.name+ ": " +secondPerson.getRate(monthlyRate)+ " Dollars");
+            System.out.print("Here is " +secondPerson.name+ "'s total monthly premium: ");
+            System.out.println(secondPerson.assumeGender(adjustRate) + secondPerson.getRate(monthlyRate));
+        System.out.println();
+
+
+        thirdPerson.display();
+        System.out.println("Adjustments: " +thirdPerson.assumeGender(adjustRate) + " Dollars");
+        System.out.println("Preliminary Rate for " +thirdPerson.name+ ": " +thirdPerson.getRate(monthlyRate)+ " Dollars");
+        System.out.print("Here is " +thirdPerson.name+ "'s total monthly premium: ");
+        System.out.println(thirdPerson.assumeGender(adjustRate) + thirdPerson.getRate(monthlyRate));
+
+
+
+
+
+
+    }
+
+}
